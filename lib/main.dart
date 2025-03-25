571d2b1de7601bb2a439532d4d4255fb91a2da65
Usama Masood <134695768+usama-devv@users.noreply.github.com>
2025-03-25 21:50:31 +0500

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Notes',
       home: const HomeScreen(),
     );
   }
