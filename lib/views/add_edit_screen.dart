fbec8e04f471a4cda3f46a84d011c465a6e42ebd
Usama Masood <134695768+usama-devv@users.noreply.github.com>
2025-03-26 00:15:28 +0500

    updated

--- lib/views/add_edit_screen.dart
+++ lib/views/add_edit_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../model/notes_model.dart';
+
 class AddEditScreen extends StatefulWidget {
-  const AddEditScreen({super.key});
+  final Note? note;
+  const AddEditScreen({super.key, this.note});
 
   @override
   State<AddEditScreen> createState() => _AddEditScreenState();
@@ -10,6 +13,6 @@ class AddEditScreen extends StatefulWidget {
 class _AddEditScreenState extends State<AddEditScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
