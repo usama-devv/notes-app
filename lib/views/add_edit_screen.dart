934012154f7724a8c3b7f19b4ffac5dc68825a93
Usama Masood <134695768+usama-devv@users.noreply.github.com>
2025-03-27 00:37:05 +0500

    saving notes working

--- lib/views/add_edit_screen.dart
+++ lib/views/add_edit_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:notes_app/services/database_helper.dart';
 
 import '../model/notes_model.dart';
@@ -29,15 +30,145 @@ class _AddEditScreenState extends State<AddEditScreen> {
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    if(widget.note != null){
-      
+    if (widget.note != null) {
+      _titleController.text = widget.note!.title;
+      _contentController.text = widget.note!.content;
+      _selectedColor = Color(int.parse(widget.note!.color));
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: Text(widget.note == null ? 'Add Note' : 'Edit Note'),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(16),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: _titleController,
+                      decoration: InputDecoration(
+                        hintText: "Title",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Please enter a title";
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                    SizedBox(height: 15),
+                    TextFormField(
+                      controller: _contentController,
+                      decoration: InputDecoration(
+                        hintText: "Content",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                      maxLines: 10,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Please enter a content";
+                        } else {
+                          return null;
+                        }
+                      },
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(16),
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.horizontal,
+                        child: Row(
+                          children:
+                              _colors.map((color) {
+                                return GestureDetector(
+                                  onTap:
+                                      () =>
+                                          setState(() => _selectedColor = color),
+                                  child: Container(
+                                    height: 40,
+                                    width: 40,
+                                    margin: EdgeInsets.only(right: 8),
+                                    decoration: BoxDecoration(
+                                      color: color,
+                                      shape: BoxShape.circle,
+                                      border: Border.all(
+                                        color:
+                                            _selectedColor == color
+                                                ? Colors.black45
+                                                : Colors.transparent,
+                                        width: 2,
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              }).toList(),
+                        ),
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        _saveNote();
+                        Get.back();
+                      },
+                      child: Container(
+                        margin: EdgeInsets.all(20),
+                        padding: EdgeInsets.all(16),
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Save Note",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+  Future<void> _saveNote() async{
+    if(_formKey.currentState!.validate()){
+      final note = Note(
+          id: widget.note?.id,
+          title: _titleController.text,
+          content: _contentController.text,
+          color: _selectedColor.value.toString(),
+          dateTime: DateTime.now().toString(),
+      );
+      if(widget.note == null){
+        await _databaseHelper.insertNote(note);
+      }else{
+        await _databaseHelper.updateNote(note);
+      }
+    }
   }
 }
