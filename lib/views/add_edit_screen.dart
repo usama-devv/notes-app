88f32f3d4dd7af8eb76098cd4bdf4ff71495ea87
Usama Masood <134695768+usama-devv@users.noreply.github.com>
2025-03-26 23:50:34 +0500

    updated

--- lib/views/add_edit_screen.dart
+++ lib/views/add_edit_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/services/database_helper.dart';
 
 import '../model/notes_model.dart';
 
@@ -11,6 +12,30 @@ class AddEditScreen extends StatefulWidget {
 }
 
 class _AddEditScreenState extends State<AddEditScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _titleController = TextEditingController();
+  final _contentController = TextEditingController();
+  final DatabaseHelper _databaseHelper = DatabaseHelper();
+  Color _selectedColor = Colors.amber;
+  final List<Color> _colors = [
+    Colors.amber,
+    Colors.greenAccent,
+    Colors.redAccent,
+    Colors.blueAccent,
+    Colors.indigo,
+    Colors.purpleAccent,
+    Colors.pinkAccent,
+  ];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    if(widget.note != null){
+      
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold();
